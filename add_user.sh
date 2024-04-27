f4c96b262b65d1fa5d589831215142308bcabd99
wngnb <125254087+wngnb@users.noreply.github.com>
2024-04-27 16:47:21 +0800

    Update add_user.sh

--- add_user.sh
+++ add_user.sh
@@ -2,59 +2,59 @@
 export MENU_OPTION="1"
 export CLIENT="HK0"
 export PASS="1"
-./wngnb/openvpn-install.sh
+./openvpn/openvpn-install.sh
 #!/bin/bash
 export MENU_OPTION="1"
 export CLIENT="HK1"
 export PASS="1"
-./wngnb/openvpn-install.sh
+./openvpn/openvpn-install.sh
 #!/bin/bash
 export MENU_OPTION="1"
 export CLIENT="HK2"
 export PASS="1"
-./wngnb/openvpn-install.sh
+./openvpn/openvpn-install.sh
 #!/bin/bash
 export MENU_OPTION="1"
 export CLIENT="HK3"
 export PASS="1"
-./wngnb/openvpn-install.sh
+./openvpn/openvpn-install.sh
 #!/bin/bash
 export MENU_OPTION="1"
 export CLIENT="HK4"
 export PASS="1"
-./wngnb/openvpn-install.sh
+./openvpn/openvpn-install.sh
 #!/bin/bash
 export MENU_OPTION="1"
 export CLIENT="HK5"
 export PASS="1"
-./wngnb/openvpn-install.sh
+./openvpn/openvpn-install.sh
 #!/bin/bash
 export MENU_OPTION="1"
 export CLIENT="HK6"
 export PASS="1"
-./wngnb/openvpn-install.sh
+./openvpn/openvpn-install.sh
 #!/bin/bash
 export MENU_OPTION="1"
 export CLIENT="HK7"
 export PASS="1"
-./wngnb/openvpn-install.sh
+./openvpn/openvpn-install.sh
 #!/bin/bash
 export MENU_OPTION="1"
 export CLIENT="HK8"
 export PASS="1"
-./wngnb/openvpn-install.sh
+./openvpn/openvpn-install.sh
 #!/bin/bash
 export MENU_OPTION="1"
 export CLIENT="HK9"
 export PASS="1"
-./wngnb/openvpn-install.sh
+./openvpn/openvpn-install.sh
 #!/bin/bash
 export MENU_OPTION="1"
 export CLIENT="HK10"
 export PASS="1"
-./wngnb/openvpn-install.sh
+./openvpn/openvpn-install.sh
 #!/bin/bash
 export MENU_OPTION="1"
 export CLIENT="HK11"
 export PASS="1"
-./wngnb/openvpn-install.sh
+./openvpn/openvpn-install.sh
